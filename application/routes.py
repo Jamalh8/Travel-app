2bd0041309efab545e72a47d2b814caf05a088ef
Jamalh8 <jamalh.888@gmail.com>
2022-06-15 13:00:21 +0100

    added create user route

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,15 @@ from application.models import User, Country, CountryVisit
 from application.forms import CreateCountry, CreateUser, Add
 from flask import render_template, redirect, url_for, request
 
-
+@app.route('/createuser', methods=['GET', 'POST'])
+def createuser():
+    form = CreateUser()
+    if form.validate_on_submit():
+        user = User(name=form.name.data, age=form.age.data, gender=form.gender.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('read'))
+    return render_template('create_user.html', form=form)
 
 
 
