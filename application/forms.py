43cdb1f8bbea4eb264ef324b1d782d43e801dbbe
Jamalh8 <jamalh.888@gmail.com>
2022-06-15 12:56:21 +0100

    added forms to add data

---
+++ application/forms.py
@@ -0,0 +1,31 @@
+from wsgiref.validate import validator
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, IntegerField
+from wtforms.validators import DataRequired, ValidationError
+from application.models import Country, User, CountryVisit
+# from wtforms_sqlalchemy.fields import QuerySelectField
+
+class CreateUser(FlaskForm):
+    name = StringField('Name :', validators=[DataRequired()])
+    age = IntegerField('Age :', validators=[DataRequired()])
+    gender = SelectField('Gender :', choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')], validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def validate_name(self, name):
+        name_object = User.query.filter_by(name=name.data).first()
+        if name_object:
+            raise ValidationError ("This user name's already taken, please pick another name.")
+
+class CreateCountry(FlaskForm):
+    name = StringField('Name: ', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def validate_name(self, name):
+        name_object = Country.query.filter_by(name=name.data).first()
+        if name_object:
+            raise ValidationError ("This country name's already exists.")
+
+class Add(FlaskForm):
+    user_name= SelectField ("User :", choices=[],validators=[DataRequired()])
+    user_country= SelectField ("Country :", choices=[],validators=[DataRequired()])
+    submit = SubmitField('Add!')
